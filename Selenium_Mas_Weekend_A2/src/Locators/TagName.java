8fe7cb3b7200b86867a29c25fbbf62b636c12f8c
Akshay Seeram <Akshay Seeram@QSpiders>
2026-03-08 12:35:39 +0530

    New Commit

---
+++ Selenium_Mas_Weekend_A2/src/Locators/TagName.java
@@ -0,0 +1,23 @@
+package Locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TagName {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demowebshop.tricentis.com/");
+
+		driver.findElement(By.tagName("input")).sendKeys("Book"+Keys.ENTER);
+
+		Thread.sleep(3000);
+		driver.quit();
+
+	}
+
+}
